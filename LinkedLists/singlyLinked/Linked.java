15da4e5e2f7011836db6bcf80d78dbb9724e4d8c
destrex271 <destrex271@gmail.com>
2022-01-31 19:03:12 +0530

    Linked List Sorted added

--- LinkedLists/singlyLinked/Linked.java
+++ LinkedLists/singlyLinked/Linked.java
@@ -1,6 +1,6 @@
+package singlyLinked;
 
 import java.io.*;
-import java.util.*;
 
 
 class Node{
@@ -29,6 +29,15 @@ public class Linked{
 		lastNode = n; // Updating the last Node element to this element
 	}
 
+	static Node searchNode(Node head, int x){
+		Node n = head;
+		while(n != null){ // Checks for end of linked list
+			if(n.data == x) return n; // Returns node if element is found
+			n = n.next;
+		}
+		return null; // Returns null if element is not found
+	} 
+
 	static void insertNode(Node head, int pos)throws IOException{
 		Node n = new Node(); // Initializing the new Node
 		System.out.println("Enter the value to store in the new Node");
@@ -40,10 +49,30 @@ public class Linked{
 	}
 
 	static void deleteNode(Node head, int pos)throws IOException{
-		Node n = new Node();
 		Node nd = getNodeAtPos(head, pos);
 		nd.next = nd.next.next; // Changing the next node to the prev node to be the node next to the one to be deleted
 		// This successfully removes the node from the linked list but the Node still exists in memory
+	}
+
+	static void updateNode(Node head, int pos, int val)throws IOException{
+		Node nd = getNodeAtPos(head, pos+1); // Getting the node
+		nd.data = val; // Changing node value
+	}
+
+	static void sortNodes(Node head)throws IOException{
+		Node i = head; // Grabbing first node
+		while(i.next != null){
+			Node j = i.next;
+			while(j != null){
+				if(i.data > j.data){ // Implementing selection sort
+					int s = i.data;
+					i.data = j.data;
+					j.data = s;
+				}
+				j = j.next;
+			}
+			i = i.next;
+		}
 	}
 
 	public static void main(String args[])throws IOException{
@@ -70,7 +99,7 @@ public class Linked{
 
 		while(true){ // Menu driven switch case to append, insert, remove and display the elements of our linked list
 
-			System.out.println("Enter :\n  1. Append Node\n  2. Insert Node\n  3. Delete Node\n  69. Exit");
+			System.out.println("Enter :\n  1. Append Node\n  2. Insert Node\n  3. Delete Node\n  4. Search For Node\n  5. Update Node\n  6. Sort Linked List\n  69. Exit");
 			int ch = Integer.valueOf(bfr.readLine());
 			int pos;
 			switch(ch){
@@ -96,10 +125,32 @@ public class Linked{
 					displayLinkedList(head);
 					System.out.println("\n-----------------------------------------------------------------");
 					break; // delete case finished
+				case 4:
+					System.out.println("Enter the element to search for in the linked list");
+					int elem = Integer.valueOf(bfr.readLine()); // Getting the element to search for
+					if(searchNode(head, elem) != null) System.out.printf("Element %d was FOUND in the linked list.",elem); // Searching for element
+					else System.out.printf("Element %d was NOT FOUND in the linked list.", elem); // And displaying appropriate messages
+					break;
+				case 5:
+				    System.out.println("Enter the position of the node to update");
+					pos = Integer.valueOf(bfr.readLine()); // Getting position of node to update
+					System.out.println("Enter the new value of the node");
+					int val = Integer.valueOf(bfr.readLine()); // Getting new value
+					updateNode(head, pos, val); // updating node
+					System.out.println("\n-----------------------------------------------------------------");
+					displayLinkedList(head);
+					System.out.println("\n-----------------------------------------------------------------");
+					break;
+				case 6: 
+					sortNodes(head); // Sort linked list
+					System.out.println("\n-----------------------------------------------------------------");
+					displayLinkedList(head); // Displaying sorted linked list
+					System.out.println("\n-----------------------------------------------------------------");
+					break;
 				case 69:
 				default:
 					System.out.println("Exiting"); // Quiting the infinite switch case
-					System.exit(0);
+					System.exit(0); // Exiting the program
 			}
 		}
 
